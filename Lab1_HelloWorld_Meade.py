b7c4a1faffbb1d44554e5da44c358f2541f8b2ae
Jiymeade <113868424+Jiymeade@users.noreply.github.com>
2025-09-25 11:02:02 -0400

    Initial commit

---
+++ Lab1_HelloWorld_Meade.py
@@ -0,0 +1,3 @@
+#Date: 9/10/2024 Name: Ji'Yahna Meade
+
+print("Ji'Yahna Meade")
---
+++ Lab2_VariableDataTypes_Meade.py
@@ -0,0 +1,10 @@
+#Date: 9/25/2025 Name: Ji'Yahna Meade
+import platform
+
+student = "Jane Doe"
+major = "Art"
+creditHours = "16"
+favoriteClass = input("Enter your favorite Class: ")
+
+print(student, "is majoring in", major, "with", creditHours, "credits",
+      "and their favorite class is", favoriteClass )
