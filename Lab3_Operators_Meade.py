7e696b96b6525580e99f28abfb02e115f0eb4db4
Jiymeade <113868424+Jiymeade@users.noreply.github.com>
2025-09-25 11:20:58 -0400

    Lab 3

---
+++ Lab3_Operators_Meade.py
@@ -0,0 +1,10 @@
+#Date: 9/25/2025 Name: Ji'Yahna Meade
+userName = ""
+result = 0
+userNum =  0
+
+userName = input("Enter your name: ")
+userNum  = int(input("Enter your number: "))
+
+result = ((userNum + 3) * 2 -4)/2 - userNum
+print(result)
